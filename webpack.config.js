6ef64b57879fa3c184c6ac8198358260a9e3e580
alext <alex.toporkov90@gmail.com>
2018-01-05 23:04:12 +0100

    package.json deprecated packages fixed

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
                 test: /(\.js)$/,
                 exclude: /node_modules/,
                 use: [
-                    {loader: 'babel-loader',options: {'presets':['es2015']}}
+                    {loader: 'babel-loader',options: {'presets':['env']}}
                 ]
             },
             {
